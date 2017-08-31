8acbc2f88d4f6c83fee23654df8dc95fbcff372f
Unknown <10775033@my.uvu.edu>
2017-08-30 20:11:17 -0600

    Added Unity project files

---
+++ hello_world/Assets/classes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classes : MonoBehaviour {
+public class One {
+
+public int money = 1000000;
+public float fuel = 0.01f;
+private string name = "Bob";
+
+}
+public class Two {
+public float fuel = 0.02f;
+private string name ="Sara";
+}
+public class Three{
+public float fuel = 0.03f;
+private string name ="Joe";
+}
+}
