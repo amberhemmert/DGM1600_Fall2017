7c1db87d15b51e84a8571073832b555451079385
Unknown <10775033@my.uvu.edu>
2017-09-20 20:08:28 -0600

    New files

---
+++ hello_world/Assets/arrays.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrays : MonoBehaviour {
+
+	public string[] heros = new string[6];
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	heros[0]="Batman";
+	heros[1]="Superman";
+	heros[2]="Wonder Woman";
+	heros[3]="Green Lantern";
+	heros[4]="Hawkman";
+	heros[5]="Bat Gitl";
+
+	print(heros[0]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
