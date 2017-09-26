918318f85c4ef773c470539fc77e2962ded847e2
Unknown <10775033@my.uvu.edu>
2017-09-25 20:05:23 -0600

    Auto stash before merge of "master" and "origin/master"
    New assignments and edits

---
+++ hello_world/Assets/Switch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+	public string townCenter = "main";
+
+	// Use this for initialization
+	void Start () {
+	switch(townCenter){
+		
+	case"main": 
+		print("Welcome to Main Street");
+
+	break;
+	case "blacksmith":
+		print("The blacksmith grumbles as you pick through the sword bin");
+		break;
+	case "bakery":
+	    print("Mmmmmmmmm baked goodness");
+	break;		
+	case "morgue":
+		print("Peeeeeeeeeeeyew! Dead things yuck!");	
+	break;
+	default: 
+		print("I don't know what you're talking about!");	
+	break;	
+	}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ hello_world/Assets/While.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While : MonoBehaviour {
+
+	public int bottles = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		while(bottles <100){
+			bottles ++;
+			print(bottles +" bottles of beer on the wall");	
+			
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
