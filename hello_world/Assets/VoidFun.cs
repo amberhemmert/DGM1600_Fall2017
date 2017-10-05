d16fd6cefe9c7c561bc0f451ecba0914acf7910c
Unknown <10775033@my.uvu.edu>
2017-10-04 20:04:22 -0600

    Void functions assignment

---
+++ hello_world/Assets/VoidFun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidFun : MonoBehaviour {
+
+public int laserBlast;
+
+	// Use this for initialization
+	void Start () {
+	DeathStar(laserBlast);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DeathStar (int vent){
+		print("That's no moon that's a space station");
+		print(vent);
+	if(vent == 1){
+		print("Porkins misses his shot");
+	}	
+	else if(vent ==2 ){	
+		print("Red leader tries run but is unsucessful");
+	}
+	else {
+		print("Death star blows up");
+	}
+	}
+}
